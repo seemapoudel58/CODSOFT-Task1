433705c072f1cb92c939a0514b9fbe5c0bea0010
seema <simapoudel6@gmail.com>
2023-09-25 19:03:24 +0545

    2nd commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const navbar = document.querySelector('#nav');
+const navBtn = document.querySelector('.nav-btn');
+const closeBtn = document.querySelector('.close-btn');
+const sideBar = document.querySelector('.sidebar');
+const date = document.querySelector('#date');
+// add fixed class to navbar
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 80) {
+    navbar.classList.add('navbar-fixed');
+  } else {
+    navbar.classList.remove('navbar-fixed');
+  }
+});
+// show sidebar
+navBtn.addEventListener('click', () => {
+  sideBar.classList.add('show-sidebar');
+});
+closeBtn.addEventListener('click', () => {
+  sideBar.classList.remove('show-sidebar');
+});
+// set year
+date.innerHTML = new Date().getFullYear();
+
+// navBtn.addEventListener('click', () => {
+//   sideBar.classList.add('show-sidebar');
+//   //   console.log(sideBar);
+// });
+// closeBtn.addEventListener('click', () => {
+//   sideBar.classList.remove('show-sidebar');
+// });
