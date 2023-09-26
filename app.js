7d2d8a9d62ed041e2d5d4bc770ebcc3e8600282a
seema <simapoudel6@gmail.com>
2023-09-26 20:47:09 +0545

    project-section-done

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const navBtn = document.querySelector('.nav-btn');
 const closeBtn = document.querySelector('.close-btn');
 const sideBar = document.querySelector('.sidebar');
 const date = document.querySelector('#date');
+const projects = document.querySelectorAll('.project');
+
+if (window.innerWidth >= 780) {
+  for (const project of projects) {
+    project.addEventListener('mouseover', () => {
+      project.style.width = '200%';
+    });
+
+    project.addEventListener('mouseout', () => {
+      project.style.width = '100%';
+    });
+  }
+}
 // add fixed class to navbar
 window.addEventListener('scroll', () => {
   if (window.pageYOffset > 50) {
